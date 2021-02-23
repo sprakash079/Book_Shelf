d0218b9a4dde0deed88f7717716f19f8f75caf4e
Prakash <prakash8128154996@gmail.com>
2021-02-23 07:06:46 +0530

    set up all

---
+++ server/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const config = require('./config/config').get(process.env.NODE_ENV);
+const app = express();
+
+
+
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+const { User } = require('./models/user');
+const { Book } = require('./models/book');
+
+
+
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//get//
+
+
+
+//post//
+app.post('/api/book', (req, res) => {
+        const book = new Book(req.body);
+        book.save((err, doc) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).json({
+                post: true,
+                bookid: doc._id
+
+            })
+        })
+
+
+    })
+    //update//
+
+//delete//
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("successfull connecect");
+
+})
